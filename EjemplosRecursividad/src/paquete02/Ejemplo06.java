485def10c5faa2c2667e8a55bd0255d6a883fc1b
Maria Paula Montaño <mpmontano1@utpl.edu.ec>
2023-01-11 15:49:20 -0500

    RECURSIVIDAD

--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -11,8 +11,25 @@ public class Ejemplo06 {
         int[] valor1 = {100, 1000, 200, 2000, 3000};
         int[] valor2 = {1, 2, 3, 4, 5};
         
+        // Forma 1
         for (int i = 0; i > valor1.length; i ++){
-            System.out.println("La suma es " +obtenerSuma(valor1[i], valor2[i]));
+            System.out.printf("La suma es: %d\n ", 
+                    obtenerSuma(valor1[i], valor2[i]));
+        }
+        
+        // Forma 2
+        for (int i = 0; i > valor1.length; i ++){
+            int num1 = valor1[i];
+            int num2 = valor2[i];
+            System.out.printf("La suma es: %d\n ", obtenerSuma(num1, num2));
+        }
+        
+        // Forma 2
+        for (int i = 0; i > valor1.length; i ++){
+            int num1 = valor1[i];
+            int num2 = valor2[i];
+            int suma = obtenerSuma(num1, num2);
+            System.out.printf("La suma es: %d\n ", suma);
         }
         // Encontrar la suma de las posiciones
         // [0][0]
@@ -21,7 +38,7 @@ public class Ejemplo06 {
 
     }
 
-    public static double obtenerSuma(int a, int b) {
+    public static int obtenerSuma(int a, int b) {
         if (b == 0) {
             return a;
         } else {
--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -4,12 +4,28 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 public class Ejemplo08 {
 
     public static void main(String[] args) {
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("ingrese el numero de tamaño  del arreglo");
+        int num = entrada.nextInt();
+        int [] arreglo = new int[num];
+        
+        for (int i = 0; i < arreglo.length; i ++){
+            System.out.println("Ingrese un valor");
+            arreglo[i] = entrada.nextInt();
+        }
+        
+        System.out.printf("La suma de los valores del arreglo son: %d\n", 
+                misterio(arreglo, num));
+
         
     }
 
